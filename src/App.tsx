57d64182d4128a63aaa81dd63b6add3ae97167bf
xuanzung0505 <truongxuandung.0.5.0.5@gmail.com>
2023-04-05 21:13:31 +0700

    husky test

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import './styles.css'
+import IMAGE from './react.png'
+import { useState } from 'react'
+// import * as dotenv from 'dotenv'
+
+// dotenv.config()
+
 export const App = () => {
-    return <h1>App.tsx</h1>
+  const [count, setCount] = useState(0)
+  const name = "hehe"
+
+  return (
+    <>
+      <h1>App.tsx</h1>
+      <img src={IMAGE} alt="react logo" width="300" height="200"></img>
+      <button
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        Button {count}
+      </button>
+    </>
+  )
 }
