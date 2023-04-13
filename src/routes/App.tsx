7bdc25c7aea0626e27b04a7f33c56528ea21b5b5
xuanzung0505 <truongxuandung.0.5.0.5@gmail.com>
2023-04-13 23:27:19 +0700

    refactor source directory

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,7 +1,7 @@
 import '../public/styles/App/App.scss'
-import { Navigation } from '../component/App/navigation_section/Navigation'
-import { PromotionSection } from '../component/App/promotion_section/PromotionSection'
-import { CategorySection } from '../component/App/category_section/Category'
+import { Navigation } from '../components/App/navigation_section/Navigation'
+import { PromotionSection } from '../components/App/promotion_section/PromotionSection'
+import { CategorySection } from '../components/App/category_section/Category'
 // import * as dotenv from 'dotenv'
 
 // dotenv.config()
