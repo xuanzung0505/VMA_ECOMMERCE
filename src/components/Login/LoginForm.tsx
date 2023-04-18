d860c044572cd332e9f54a1a7f2d4367f8abda9a
xuanzung0505 <truongxuandung.0.5.0.5@gmail.com>
2023-04-18 21:05:40 +0700

    finish /login UI

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,5 +1,6 @@
 import '../../public/styles/Login/LoginForm.scss'
 import formBackground from '../../public/assets/Shopee-Login-BG.png'
+import '../../public/styles/common.scss'
 
 const LoginForm = () => {
   return (
@@ -11,7 +12,52 @@ const LoginForm = () => {
         className="form__banner login"
         style={{ backgroundImage: `url(${formBackground})` }}
       ></div>
-      <form action="/api/login">Đăng nhập</form>
+      <div className="form__content login">
+        <div className="form__header">Đăng nhập</div>
+        <form action="/api/login">
+          <div className="form__body">
+            <input
+              type="text"
+              placeholder="Email/Số điện thoại/Tên đăng nhập"
+            ></input>
+            <input type="password" placeholder="Mật khẩu"></input>
+            <input type="submit" value={'ĐĂNG NHẬP'}></input>
+          </div>
+        </form>
+        <div className="form__row">
+          <a href="#">Quên mật khẩu</a>
+          <a href="#">Đăng nhập với SMS</a>
+        </div>
+        <div className="form__row">
+          <div className="divider-horizontal"></div>
+          HOẶC
+          <div className="divider-horizontal"></div>
+        </div>
+        <div className="form__row">
+          <button className="btn-type1">
+            <i
+              className="fa-brands fa-facebook fa-xl"
+              style={{ color: '#005eff' }}
+            ></i>
+            Facebook
+          </button>
+          <button className="btn-type1">
+            <i
+              className="fa-brands fa-google fa-xl"
+              style={{ color: '#ff0000' }}
+            ></i>
+            Google
+          </button>
+        </div>
+        <div className="form__row center CommonPaddingTop-1">
+          <div>
+            Bạn mới biết đến Shopee?
+            <a href="#" className="CommonTextColor-1 CommonFontSize-1">
+              Đăng ký
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Login/Navigation.tsx
+++ src/components/Login/Navigation.tsx
@@ -4,16 +4,18 @@ const Navigation = () => {
   return (
     <div className="navigation__container login">
       <div className="navigation login">
-        <div className="navigation__left login">
-          <div className="navigation__body__logo">
-            <a href="https://shopee.vn">
-              <img src={shopee_url}></img>
-            </a>
+        <div className="navigation__header">
+          <div className="navigation__header__left login">
+            <div className="navigation__body__logo login">
+              <a href="https://shopee.vn">
+                <img src={shopee_url}></img>
+              </a>
+            </div>
+            <div className="bigText">Đăng nhập</div>
           </div>
-          <div className="bigText login">Đăng nhập</div>
-        </div>
-        <div className="navigation__right login">
-          <a href="#">Bạn cần giúp đỡ?</a>
+          <div className="navigation__header__right login">
+            <a href="#">Bạn cần giúp đỡ?</a>
+          </div>
         </div>
       </div>
     </div>
